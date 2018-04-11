d3cf23b6148c584dbebf043aac870d383110abbf
Grzegorz Pawlik <grzegorzpawlik@gmail.com>
2018-04-11 19:46:42 +0200

    Check if works with `_spec`

--- src/complement.ts
+++ src/complement.ts
@@ -1,12 +1,12 @@
 export default function mirror(name: string): string {
-  if (name.includes('.spec.')) {
-    return name.replace(/\.spec\./, '.');
+  if (name.includes('_spec.')) {
+    return name.replace(/_spec\./, '.');
   }
 
   const dot = name.lastIndexOf('.');
 
   if (dot !== -1) {
-    return `${name.substring(0, dot)}.spec${name.substring(dot)}`;
+    return `${name.substring(0, dot)}_spec${name.substring(dot)}`;
   }
 
   return undefined;
